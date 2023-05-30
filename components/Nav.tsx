e2914340b7bdafb15d3ec1366e65361f890f787d
MingmaTenzing <pasangsherpa689@gmail.com>
2023-05-30 14:17:17 +0800

    contact page complete

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -10,15 +10,19 @@ function Nav({}: Props) {
     <div className='p-4 border-b sticky top-0 z-10 bg-white  '>
         <div className=' lg:hidden flex items-center justify-between'>
             <Bars3Icon className='w-6 text-gray-600' />
-            <Image src={logo} alt='B2Creative Logo' width={200} height={200} className='  rounded-full w-14' />
+         <Link href="/">
+          <Image src={logo} alt='B2Creative Logo' width={200} height={200} className='  rounded-full w-14' />
+         </Link>  
             <PhoneIcon className='w-6 text-gray-600' />
         </div>
 
         <div  className='  px-[200px] items-center hidden lg:flex justify-between '>
-        <Image src={logo} alt='B2Creative Logo' width={200} height={200} className='  rounded-full w-14' />
+       <Link href="/">
+       <Image src={logo} alt='B2Creative Logo' width={200} height={200} className='  rounded-full w-14' />
+       </Link> 
         <div className='flex items-center space-x-10  text-sm text-gray-600 font-semibold'>
-            <Link href="">About</Link>
-            <Link href="" className=' border border-black  px-4 py-3 hover:bg-green-500 hover:border-green-500 transition-all ease-linear duration-200 hover:text-white  rounded-md'>Contact Us</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact" className=' border border-black  px-4 py-3 hover:bg-green-500 hover:border-green-500 transition-all ease-linear duration-200 hover:text-white  rounded-md'>Contact Us</Link>
         </div>
         </div>
     </div>
--- utils/Question.tsx
+++ utils/Question.tsx
@@ -4,8 +4,8 @@ import {
   ChevronUpIcon,
 } from "@heroicons/react/24/outline";
 import { useEffect, useState } from "react";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export interface questiontype {
   title: string;
@@ -20,32 +20,37 @@ interface Props {
 function Question({ item }: Props) {
   const [openModal, setOpenModal] = useState<boolean>(false);
 
-
   useEffect(() => {
     AOS.init();
-  },[])
+  }, []);
   return (
     <div className=" md:w-[600px] m-auto">
-        <div className="m-4 cursor-pointer">
-
-
-
-      <div onClick={() => setOpenModal(!openModal)}  className=" flex items-center  justify-between border p-4 rounded-lg bg-gray-200 bg-opacity-40 ">
-        <p className="  font-semibold">{item.title}</p>
-        {openModal ? (
-          <ChevronUpIcon data-aos="flip-down" className=" w-6" onClick={() => setOpenModal(!openModal)}  />
-        ) : (
-          <ChevronDownIcon data-aos="flip-up" className=" w-6" onClick={() => setOpenModal(!openModal)} />
+      <div className="m-4 cursor-pointer">
+        <div
+          onClick={() => setOpenModal(!openModal)}
+          className=" flex items-center  justify-between border p-4 rounded-lg bg-gray-200 bg-opacity-40 "
+        >
+          <p className="  font-semibold">{item.title}</p>
+          {openModal ? (
+            <ChevronUpIcon
+              data-aos="flip-down"
+              className=" w-6"
+              onClick={() => setOpenModal(!openModal)}
+            />
+          ) : (
+            <ChevronDownIcon
+              data-aos="flip-up"
+              className=" w-6"
+              onClick={() => setOpenModal(!openModal)}
+            />
+          )}
+        </div>
+        {openModal && (
+          <div data-aos="fade-down" data-aos-once="true" className=" p-4">
+            <p className="   text-sm font-light text-gray-500">{item.answer}</p>
+          </div>
         )}
       </div>
-      {
-        openModal &&  <div data-aos="fade-down"
-        data-aos-once="true" className=" p-4">
-        <p  className="  text-sm font-light text-gray-500">{item.answer}</p>
-      </div>
-      }
-        </div>
-     
     </div>
   );
 }
