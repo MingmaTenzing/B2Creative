0352eea107fcfb997f138b8c37ec5dba88f7c5a4
MingmaTenzing <pasangsherpa689@gmail.com>
2023-05-31 14:07:16 +0800

    fully functional now need customer review

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import logo from "../assests/b2creative logo.jpg";
 import Link from "next/link";
+import instagram from "../assests/instagram.png";
+import facebook from "../assests/facebook.png";
+import tiktok from "../assests/tik-tok.png";
 
 type Props = {};
 function Footer({}: Props) {
@@ -16,48 +19,66 @@ function Footer({}: Props) {
             className=" w-16"
           />
           <p className=" text-center font-light text-gray-600 ">
-        Your digital marketing partner for success. 
+            Your digital marketing partner for success.
           </p>
         </div>
 
         <div className=" mt-8 space-y-4 md:space-y-0 md:flex md:space-x-40">
           <div className=" space-y-3 flex-col flex ">
             <h3 className=" font-semibold text-lg">Quick Links</h3>
-            <Link href="/about" className=" font-light">
+            <Link target="_blank" href="/about" className=" font-light">
               About Us
             </Link>
-            <p className=" cursor-pointer  font-light">
-              Case Studies
-            </p>
-            <p className=" font-light">
-              Lawyer SEO
-            </p>
-            <p className=" font-light">
-              Testimonials
-            </p>
+            <p className=" cursor-pointer  font-light">Case Studies</p>
+            <p className=" font-light">Lawyer SEO</p>
+            <p className=" font-light">Testimonials</p>
           </div>
           <div className=" space-y-3 flex-col flex ">
             <h3 className=" font-semibold text-lg">Featured Services</h3>
-            <p className=" font-light">
-              Local SEO
-            </p>
-            <p className=" font-light">
-              Social Media Marketing
-            </p>
-            <p className=" font-light">
-              SEO Optimization
-            </p>
-            <p className=" font-light">
-              Pay Per Click Management
-            </p>
+            <p className=" font-light">Local SEO</p>
+            <p className=" font-light">Social Media Marketing</p>
+            <p className=" font-light">SEO Optimization</p>
+            <p className=" font-light">Pay Per Click Management</p>
           </div>
 
-          <div className=" space-y-2">
+          <div className=" space-y-4">
             <h3 className=" font-semibold text-lg">Follow the Best</h3>
             <input
               className=" border outline-none p-4 w-full"
               placeholder="Email Address"
             ></input>
+            <div className="  space-y-2">
+              <h3 className=" font-semibold text-lg">Find Us On</h3>
+              <div className=" flex items-center space-x-3">
+                <Link target="_blank" href="https://www.instagram.com/b2creative.au/">
+                  <Image
+                    src={instagram}
+                    height={200}
+                    width={200}
+                    alt="instagram b2creative"
+                    className=" w-10 hover:scale-110 transition-all ease-linear duration-150"
+                  />
+                </Link>
+                <Link target="_blank" href="https://www.facebook.com/profile.php?id=100091245305321">
+                  <Image
+                    src={facebook}
+                    height={200}
+                    width={200}
+                    alt="instagram b2creative"
+                    className=" w-10 hover:scale-110 transition-all ease-linear duration-150"
+                  />
+                </Link>
+                <Link  target="_blank" href="https://www.tiktok.com/@b2creative.au?is_from_webapp=1&sender_device=pc">
+                  <Image
+                    src={tiktok}
+                    height={200}
+                    width={200}
+                    alt="instagram b2creative"
+                    className=" w-10 hover:scale-110 transition-all ease-linear duration-150"
+                  />
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,31 +1,73 @@
-import { Bars3Icon, PhoneIcon } from '@heroicons/react/24/outline'
-import Image from 'next/image'
-import logo from "../assests/b2creative logo.jpg"
-import Link from 'next/link'
+"use client";
+import { Bars3Icon, PhoneIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import logo from "../assests/b2creative logo.jpg";
+import Link from "next/link";
+import { useState } from "react";
 
-
-type Props = {}
+type Props = {};
 function Nav({}: Props) {
+  const [modal, setModal] = useState<boolean>(false);
   return (
-    <div className='p-4 border-b sticky top-0 z-10 bg-white  '>
-        <div className=' lg:hidden flex items-center justify-between'>
-            <Bars3Icon className='w-6 text-gray-600' />
-         <Link href="/">
-          <Image src={logo} alt='B2Creative Logo' width={200} height={200} className='  rounded-full w-14' />
-         </Link>  
-            <PhoneIcon className='w-6 text-gray-600' />
-        </div>
+    <div className="  p-4 border-b sticky top-0 z-10 bg-white  ">
+      <div className=" md:hidden flex items-center justify-between">
+        <Bars3Icon
+          onClick={() => setModal(!modal)}
+          className="w-6 text-gray-600"
+        />
+        <Link href="/">
+          <Image
+            src={logo}
+            alt="B2Creative Logo"
+            width={200}
+            height={200}
+            className="  rounded-full w-14"
+          />
+        </Link>
+       <Link href="/contact" >
+         <PhoneIcon className="w-6 text-gray-600" />
+        </Link>
+      </div>
 
-        <div  className='  px-[200px] items-center hidden lg:flex justify-between '>
-       <Link href="/">
-       <Image src={logo} alt='B2Creative Logo' width={200} height={200} className='  rounded-full w-14' />
-       </Link> 
-        <div className='flex items-center space-x-10  text-sm text-gray-600 font-semibold'>
+      <div className="  md:px-[100px] lg:px-[200px] items-center hidden md:flex justify-between ">
+        <Link href="/">
+          <Image
+            src={logo}
+            alt="B2Creative Logo"
+            width={200}
+            height={200}
+            className="  rounded-full w-14"
+          />
+        </Link>
+        <div className="flex items-center space-x-10  text-sm text-gray-600 font-semibold">
+          <Link href="/about">About</Link>
+          <Link
+            href="/contact"
+            className=" border border-black  px-4 py-3 hover:bg-green-500 hover:border-green-500 transition-all ease-linear duration-200 hover:text-white  rounded-md"
+          >
+            Contact Us
+          </Link>
+        </div>
+      </div>
+
+      {modal && (
+        <div data-aos="fade-right" data-aos-duration="300"  className=" p-4 h-[100vh] w-[200px] left-0 top-0  bg-gray-100 absolute">
+        <div className=" flex justify-end ">
+          <XMarkIcon className=" w-8" onClick={() => setModal(!modal)} />
+          </div>
+
+          <div className=" flex flex-col space-y-3 font-semibold text-lg text-gray-700 items-center justify-center h-full" >
+            <Link href="/">Home</Link>
             <Link href="/about">About</Link>
-            <Link href="/contact" className=' border border-black  px-4 py-3 hover:bg-green-500 hover:border-green-500 transition-all ease-linear duration-200 hover:text-white  rounded-md'>Contact Us</Link>
+            <Link href="/contact">Contact</Link>
+
+            </div>
+
+
+
         </div>
-        </div>
+      )}
     </div>
-  )
+  );
 }
-export default Nav
+export default Nav;
